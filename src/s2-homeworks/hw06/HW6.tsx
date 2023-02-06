c0e643675ac7570cdd111dee9c4108945d4997cf
ShavelD <legrand.97@mail.ru>
2023-02-06 17:02:19 +0300

    hw6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
-import { restoreState, saveState } from './localStorage/localStorage'
+import {restoreState, saveState} from './localStorage/localStorage'
 import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s from './HW6.module.css'
@@ -20,13 +20,16 @@ const HW6 = () => {
     const restore = () => {
         // делают студенты
         // ЗАСЕТАТЬ(restoreState<ТИПИЗАЦИЯ>(КЛЮЧ,''))
+        const state: string = restoreState<string>('hw6-editable-span-value', '')
+        setValue(state)
     }
 
     return (
         <div id={'hw6'}>
             <div className={s2.hwTitle}>Homework #6</div>
-
-            {/*демонстрация возможностей компоненты:*/}
+            <div>
+                <hr></hr>
+            </div>
             <div className={s2.hw}>
                 <div className={s.editableSpanContainer}>
                     <SuperEditableSpan
